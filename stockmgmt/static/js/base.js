17515bfebfc1a0abd185f675001bd965d90a1351
adnanmuhtadi <adnanmuhtadi@hotmail.com>
2023-08-29 12:48:38 +0100

    added styling to the date picker and progress load bar

--- stockmgmt/static/js/base.js
+++ stockmgmt/static/js/base.js
@@ -1,3 +1,21 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+
+$(document).ready(function () {
+
+  NProgress.start();
+  NProgress.done();
+
+  $(".datetimeinput").datepicker({
+      changeYear: true,
+      changeMonth: true,
+      dateFormat: 'yy-mm-dd'
+  });
+});
+
+
+
+
